3aaff58cee6af107443800f4db702f10efd9b9ef
Ronan Hughes <rhughes490@gmail.com>
2020-09-16 09:46:00 +0100

    Add audio controls

--- src/components/Song.js
+++ src/components/Song.js
@@ -7,7 +7,8 @@ const Song = (props) => {
                 <img src={props.image} alt="" className="album-cover"/>
                 <h3 className="title">{props.title}</h3>
                 <h3 className="artist">{props.artist}</h3>
-                <a href={props.preview} className="link">Play</a>
+                <audio controls src={props.preview}> </audio>
+                <hr></hr>
             </li>
         </div>
     )
